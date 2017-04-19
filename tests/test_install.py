d09304191a1567c78e614e7bb5e1e59f934e4a6b
Matthieu REMY <marem@smile.fr>
2017-04-19 17:15:31 +0200

    Install java

--- tests/test_install.py
+++ tests/test_install.py
@@ -4,9 +4,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_hosts_file(File):
-    f = File('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_application_running(Service):
+    application = Service("spring-boot-sample")
+    assert application.is_enabled
